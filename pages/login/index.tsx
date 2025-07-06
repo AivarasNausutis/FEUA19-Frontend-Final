5972afb7dc3af206bb10cfc380e3946438a73ac1
AivarasNausutis <aivaras078@gmail.com>
2025-07-06 22:49:37 +0300

    started working on login form and signup

---
+++ pages/login/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/LoginForm";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+export default index;
---
+++ pages/signup/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const signUp = () => {
+  return <div>signup</div>;
+};
+
+export default signUp;
